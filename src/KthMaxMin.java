3d3c2c8aad0327d9d8312dddfde924f721d28be6
Charu <charut626@gmail.com>
2024-01-11 22:20:06 +0530

    3.Kth max & min element of an array.

---
+++ src/KthMaxMin.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class KthMaxMin {
+    public static void main(String[] args) {
+        int[] arr = {20,10,60,30,50,40};
+        int k = 3;
+        System.out.println(kthMax(arr,k));
+        System.out.println(kthMin(arr,k));
+    }
+        static int kthMax( int[] arr, int k){
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for (int i = 0; i < k; i++) {
+                pq.add(arr[i]);
+            }
+            for (int i = k; i < arr.length; i++) {
+                if (pq.peek() < arr[i]) {
+                    pq.poll();
+                    pq.add(arr[i]);
+                }
+            }
+            return pq.peek();
+        }
+    static int kthMin( int[] arr, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < k; i++) {
+            pq.add(arr[i]);
+        }
+        for (int i = k; i < arr.length; i++) {
+            if (pq.peek() > arr[i]) {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.peek();
+    }
+    }
